474399dd0a0d41f151f1921b1b3b9f57245540e2
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2022-05-10 16:18:42 -0400

    signup style

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,67 +1,52 @@
 import * as React from "react";
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import shelfLogo from './shelfLogo.png'
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { flexbox } from "@mui/system";
+
 
 const card = (
   <React.Fragment>
     <CardContent>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <AccountCircleIcon sx={{ width: "150px", height: "150px", color: "white" }} />
-      </Box>
-      <Box sx={{ display: "flex", justifyContent: "center", ml:3, mt:10 }}>
+      <Box sx={{ display: "flex", justifyContent: "center", ml:3, mt:5, justifyContent: 'space-evenly'  }}>      
         <div>
-          <TextField
-            disabled={false}
-            id="filled"
-            variant="filled"
-            label="First Name"
-            // defaultValue={editedItem?.Name}
-            sx={{ backgroundColor: "white", borerRadius: "5" }}
-            // onChange={(e) =>
-            //   setNewValue({ ...newValue, Name: e.target.value })
-            // }
-          />
-          <TextField
-            id="filled"
-            variant="filled"
-            label="Last Name"
-            // defaultValue={editedItem?.NSN}
-            sx={{ backgroundColor: "white", borerRadius: "5" }}
-            // onChange={(e) =>
-            //   setNewValue({ ...newValue, NSN: e.target.value })
-            // }
-          />
+        <Box sx={{ '& > :not(style)': { m: 1 } }}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField required={true} id="input-with-sx" label="Your Name" variant="standard" />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <CreditCardOutlinedIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField required={true} id="input-with-sx" label="Your DOD-ID" variant="standard" />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <EmailIcon  sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField required={true} id="input-with-sx" label="Your E-mail" variant="standard" />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <LockIcon  sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField  required={true} id="input-with-sx" label="Password" variant="standard" />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <LockOutlinedIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+            <TextField required={true}  id="input-with-sx" label="Repeat Password" variant="standard" />
+          </Box>
+        </Box>
         </div>
-        <div>
-          <TextField
-            disabled={false}
-            id="filled"
-            variant="filled"
-            label="DOD ID"
-            // defaultValue={editedItem?.Name}
-            sx={{ backgroundColor: "white", borerRadius: "5" }}
-            // onChange={(e) =>
-            //   setNewValue({ ...newValue, Name: e.target.value })
-            // }
-          />
-          <TextField
-            id="filled"
-            variant="filled"
-            label="Email Address"
-            // defaultValue={editedItem?.NSN}
-            sx={{ backgroundColor: "white", borerRadius: "5" }}
-            // onChange={(e) =>
-            //   setNewValue({ ...newValue, NSN: e.target.value })
-            // }
-          />
-        </div>
+
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 7 }}>
+          <img alt="shelf logo" src={shelfLogo} width='200' height='200'/>
+        </Box>
       </Box>
     </CardContent>
     <CardActions>
@@ -83,7 +68,7 @@ export default function SignUp() {
     <Box
       sx={{ display: "flex", justifyContent: "center", alignContent: "center", mt:10 }}
     >
-      <Card sx={{ width: "500px", bgcolor: "#1A73E8" }} variant="outlined">
+      <Card sx={{ width: "700px" }} variant="outlined">
         {card}
       </Card>
     </Box>
