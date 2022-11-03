d5b8496b2d1cea044af71601927ad9807219419e
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2022-11-03 13:16:28 -0400

    working on sql for warehouse

--- server/user_routes/userRoutes.js
+++ server/user_routes/userRoutes.js
@@ -31,7 +31,7 @@ async function getUsers(req, res) {
 async function addUser(req, res) {
   console.log("req.body", req.body);
   pool.query(
-    `INSERT INTO users (dod_id, first_name, last_name, email, ima, organization, warehouse_access) values('${req.body.users.dod_id}', '${req.body.users.first_name}', '${req.body.users.last_name}', '${req.body.users.email}', '${req.body.users.ima}', '${req.body.users.organization}', '{"Name": "${req.body.users.organization}"}'::jsonb)`,
+    `INSERT INTO users (dod_id, first_name, last_name, email, ima, organization, warehouse_access) values('${req.body.users.dod_id}', '${req.body.users.first_name}', '${req.body.users.last_name}', '${req.body.users.email}', '${req.body.users.ima}', '${req.body.users.organization}', '[{"Name": "${req.body.users.organization}"}]'::jsonb)`,
     (error, results) => {
       if (error) {
         return res.send("error" + error);
