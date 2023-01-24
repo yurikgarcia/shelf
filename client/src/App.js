4ef71f597958dda137471fe2342b9fc17e335c16
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2023-01-24 16:46:39 -0500

    url

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ function App() {
 
 
   const API = {
-    website: 'http://shelfapi-env.eba-phxch2is.us-gov-west-1.elasticbeanstalk.com/'
+    website: 'http://Shelfapi-env.eba-phxch2is.us-gov-west-1.elasticbeanstalk.com/'
   };
 
   
